2d7cb2471f758b8a54f2a63964d8471adb5a738d
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-12 20:57:47 -0300

    New Commit

---
+++ src/studies/dev/learningjava/introduction/C05P4conditionalStatements.java
@@ -0,0 +1,22 @@
+package studies.dev.learningjava.introduction;
+
+public class C05P4conditionalStatements {
+    public static void main(String[] args) {
+
+        double annualSalary = 70000;
+        double firstBracket = 9.70 / 100;
+        double secondBracket = 37.35 / 100;
+        double thirdBracket = 49.50 / 100;
+        double amountToBePaid;
+        if (annualSalary < 34712){
+            amountToBePaid = annualSalary * firstBracket;
+        }
+        else if (annualSalary >= 34713 && annualSalary <= 68587) {
+            amountToBePaid = annualSalary * secondBracket;
+        }
+        else {
+            amountToBePaid = annualSalary * thirdBracket;
+        }
+        System.out.println(amountToBePaid);
+    }
+}
