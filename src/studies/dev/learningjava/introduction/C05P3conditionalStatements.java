7495a1f3f591e5a646f89fe90f0cf635a59bf9b9
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-12 20:43:06 -0300

    New Commit

---
+++ src/studies/dev/learningjava/introduction/C05P3conditionalStatements.java
@@ -0,0 +1,15 @@
+package studies.dev.learningjava.introduction;
+
+public class C05P3conditionalStatements {
+    public static void main(String[] args) {
+
+        // if salary > 2000 "buy laptop" "don't buy laptop"
+        double salary = 35880;
+        String shouldBuyMessage = "buy laptop";
+        String shouldNotBuyMessage = "don't buy laptop";
+        // (condition) ? <true> : <false>
+        String displayMessage = salary > 2000 ? shouldBuyMessage : shouldNotBuyMessage;
+
+        System.out.println(displayMessage);
+    }
+}
