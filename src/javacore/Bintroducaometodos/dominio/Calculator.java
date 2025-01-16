ef0c98534e721aa91d3a73f62ffcc7ada84685e9
Raissa <raissaassissantos7@gmail.com>
2025-01-15 23:13:07 -0300

    Progress

--- src/javacore/Bintroducaometodos/dominio/Calculator.java
+++ src/javacore/Bintroducaometodos/dominio/Calculator.java
@@ -20,4 +20,26 @@ public class Calculator {
         }
         return num1/num2;
     }
+
+    public double divideTwoNumbers02(double num1, double num2){
+        if (num2 != 0){
+            return num1/num2;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void printDivisionOfTwoNumbers(double num1, double num2){
+        if(num2 == 0){
+            System.out.println("No division by 0");
+            return;
+        }
+        System.out.println(num1/num2);
+    }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+    }
 }
---
+++ src/javacore/Bintroducaometodos/test/CalculatorTest04.java
@@ -0,0 +1,12 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Calculator;
+
+public class CalculatorTest04 {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int num1 = 1;
+        int num2 = 2;
+        calculator.alteraDoisNumeros(num1, num2);
+    }
+}
