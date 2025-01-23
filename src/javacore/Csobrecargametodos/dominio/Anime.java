3bafdc7827a7c81ddd3b652f72d9111649c5be3b
Raissa <raissaassissantos7@gmail.com>
2025-01-22 23:19:27 -0300

    Progress

--- src/javacore/Csobrecargametodos/dominio/Anime.java
+++ src/javacore/Csobrecargametodos/dominio/Anime.java
@@ -4,11 +4,23 @@ public class Anime {
     private String name;
     private String type;
     private int episodes;
+    private String gender;
 
+    public void init(String name, String type, int episodes){
+        this.name = name;
+        this.type = type;
+        this.episodes = episodes;
+    }
+
+    public void init(String name, String type, int episodes, String gender){
+        this.init(name, type, episodes);
+        this.gender = gender;
+    }
     public void imprime(){
         System.out.println(this.name);
         System.out.println(this.type);
         System.out.println(this.episodes);
+        System.out.println(this.gender);
     }
 
     public String getName() {
@@ -31,5 +43,13 @@ public class Anime {
 
     public int getEpisodes() {
         return episodes;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
--- src/javacore/Csobrecargametodos/test/AnimeTest01.java
+++ src/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -5,9 +5,8 @@ import javacore.Csobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.setName("The Dark Princess");
-        anime.setType("TV");
-        anime.setEpisodes(12);
+        anime.init("The Dark Princess", "TV", 12, "Adventure");
+        anime.setGender("Adventure");
         anime.imprime();
 
     }
