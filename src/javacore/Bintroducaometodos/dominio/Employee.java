8413869f9b197082e76287fca3d899ccd4536931
Raissa <raissaassissantos7@gmail.com>
2025-01-22 22:43:29 -0300

    Class 56

--- src/javacore/Bintroducaometodos/dominio/Employee.java
+++ src/javacore/Bintroducaometodos/dominio/Employee.java
@@ -1,9 +1,10 @@
 package javacore.Bintroducaometodos.dominio;
 
 public class Employee {
-    public String name;
-    public int age;
-    public double[] salaries;
+    private String name;
+    private int age;
+    private double[] salaries;
+    private double average;
 
     public void  print(){
         System.out.println(this.name);
@@ -17,11 +18,38 @@ public class Employee {
     }
 
     public void printAverage(){
-        double average = 0;
         for (double salary: salaries){
             average += salary;
         }
         average /= salaries.length;
         System.out.println("\nAverage: "+ average);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalaries(double[] salaries) {
+        this.salaries = salaries;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double[] getSalaries() {
+        return salaries;
+    }
+
+    public double getAverage() {
+        return average;
     }
 }
--- src/javacore/Bintroducaometodos/test/EmployeeTest01.java
+++ src/javacore/Bintroducaometodos/test/EmployeeTest01.java
@@ -6,11 +6,12 @@ import javacore.Bintroducaometodos.dominio.Employee;
 public class EmployeeTest01 {
     public static void main(String[] args) {
         Employee employee = new Employee();
-        employee.name = "Eli";
-        employee.age = 24;
-        employee.salaries = new double[]{1200, 987.32, 2500};
+        employee.setName("Eli");
+        employee.setAge(24);
+        employee.setSalaries(new double[]{1200, 987.32, 2500});
 
         employee.print();
+        System.out.println(employee.getAverage());
 
     }
 }
